1fb8ff1a9bc45460f2cea55ce26dc8f796618a0d
David Malmström <syntex@outlook.com>
2021-05-14 01:22:49 +0200

    purge only in prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const enablePurge = process.env.NODE_ENV === 'production';
-console.log('process.env.NODE_ENV', !!process.env.NODE_ENV);
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 module.exports = {
   purge: {
-    enabled: enablePurge,
+    enabled: process.env.NODE_ENV === 'production',
     content: ['./src/**/*.html', './src/**/*.scss'],
   },
   darkMode: 'class', // or 'media' or 'class'
