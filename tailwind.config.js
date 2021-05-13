164c5346c1bdaac985181a5546c7917a3245f6ce
David Malmström <syntex@outlook.com>
2021-05-14 01:14:48 +0200

    log NODE_ENV

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const enablePurge = process.env.NODE_ENV === 'production';
-
+console.log('process.env.NODE_ENV', !!process.env.NODE_ENV);
 module.exports = {
   purge: {
     enabled: enablePurge,
