f9f3cb1d27cfefff6a320d33c34fc025ecccefc9
David Malmström <syntex@outlook.com>
2021-05-14 01:30:18 +0200

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,21 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 import { AppComponent } from './app.component';
+import { HoldingService } from './shared/services/holding.service';
+import { TableService } from './shared/services/table.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent]
-    }).compileComponents();
+    component = new AppComponent(
+      {
+        getHoldings: jest.fn().mockReturnValue(of([])),
+        holdings$: of([]),
+      } as HoldingService,
+      {} as TableService
+    );
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'syr-table'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('syr-table');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('p').textContent).toContain('Thanks for reading!');
+    expect(component).toBeTruthy();
   });
 });
